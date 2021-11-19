7d75151367a4637b4f54e470c4f8af324fd34f55
UkhtamovSH <uktamovshokhrukh@gmail.com>
2021-11-19 17:11:52 +0500

    login in progress 1

---
+++ src/components/LogRegEntrance.jsx
@@ -0,0 +1,11 @@
+import { LogRegEntranceStyle } from "../styles/LogRegEntrance.styled"
+
+const LogRegEntrance = () => {
+  return (
+    <LogRegEntranceStyle>
+      gg
+    </LogRegEntranceStyle>
+  )
+}
+
+export default LogRegEntrance
--- src/router/Routess.jsx
+++ src/router/Routess.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import LogRegEntrance from '../components/LogRegEntrance'
 import Section from '../components/Section'
 
 const Routess = () => {
   return (
     <Routes>
       <Route path="/" element={<Section />} />
+      <Route path="/log-in" element={<LogRegEntrance />} />
     </Routes >
   )
 }
